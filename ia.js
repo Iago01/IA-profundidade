10575aaac87e976bb9bed0039b35abf18f429e0d
Iago01 <emerson_iago@hotmail.com>
2022-03-20 22:10:58 -0300

    Update ia.js

--- ia.js
+++ ia.js
@@ -6,12 +6,12 @@ let grafo = [
     },
     {
       'nome':"B",
-      'filhos' : ["E","F","C"],
+      'filhos' : ["E","F"],
       'escolhido':false
     },
     {
       'nome':"C",
-      'filhos' : ["G"],
+      'filhos' : ["F","G"],
       'escolhido':false
     },
     {
@@ -21,21 +21,21 @@ let grafo = [
     },
     {
       'nome':"E",
-      'filhos' : [],
+      'filhos' : ["H","I"],
       'escolhido':false
     },
     {
       'nome':"F",
-      'filhos' : [],
+      'filhos' : ["J"],
       'escolhido':false
     },
     {
       'nome':"G",
-      'filhos' : ["H"],
-      'escolhido': false
+      'filhos' : [""],
+      'escolhido': true
     },{
       'nome':"H",
-      'filhos' : ["I","J"],
+      'filhos' : [],
       'escolhido': false
     },
     {
@@ -46,7 +46,7 @@ let grafo = [
     {
       'nome':"J",
       'filhos' : [],
-      'escolhido': true
+      'escolhido': false
     },
     ]; 
     function retornaNoPorNome(nome,grafo){
@@ -59,25 +59,22 @@ let grafo = [
     function semFilhos(no,LE,LNE,BSS){
       let noAlterado = no;
       if(noAlterado.filhos.length == 0){
-        console.log("retornou true de primeira");
         return true
       }
       else{
-        console.log(noAlterado);
         for(let i = 0;i< noAlterado.filhos.length;i++){
           for(let j = 0; j < LE.length ; j++){
             if(noAlterado.filhos[i]==LE[j].nome){
               let myIndex = noAlterado.filhos.indexOf(noAlterado.filhos[i]);
               noAlterado.filhos.splice(myIndex,1);
-              console.log(noAlterado);
+              
             }
           }
           for(let l = 0; l < LNE.length ; l++ ){
             if(noAlterado.filhos[i]==LNE[l].nome){
-              console.log(noAlterado.filhos.indexOf(noAlterado.filhos[i]))
               let myIndex = noAlterado.filhos.indexOf(noAlterado.filhos[i]);
               noAlterado.filhos.splice(myIndex,1);
-              console.log(noAlterado);
+              
             }
           }
           if(BSS.length != 0){
@@ -85,36 +82,48 @@ let grafo = [
               if(noAlterado.filhos[i]==BSS[m].nome){
                 let myIndex = noAlterado.filhos.indexOf(noAlterado.filhos[i]);
                 noAlterado.filhos.splice(myIndex,1);
-                console.log(noAlterado);
+              
               }
             }
           } 
         }
         if(noAlterado.length == 0){
-          console.log("retornou true de segunda");
+          
           return true;
         }
         else{
-          console.log("retornou false");
+          
           return noAlterado.filhos;
         }
       }
+    }
+    function imprimirDados(dados){
+      let texto = "";
+      for(let i = 0;i<dados.length;i++){
+        texto = texto + " " + dados[i].nome
+      }
+      return texto
     }
     function procurarNo(grafo){
       let LE = [grafo[0]];
       let LNE = [grafo[0]];
       let BSS = [];
       let EC = grafo[0];
+      let iteracao = 0
       while(LE.length != 0 ){
-        console.log("PRIMEIRO DE LE: " + LE[0].nome)
-        console.log("PRIMEIRO DE LNE: "+LNE[0].nome)
-        console.log("EC ESCOLHIDO: " + EC.nome)
+        console.log("ITERAÇÃO " + iteracao)
+        console.log("LE : " + imprimirDados(LE))
+        console.log("LNE : " + imprimirDados(LNE))
+        console.log("BSS : " + imprimirDados(BSS))
+        console.log("EC : " + EC.nome)
+        console.log("")
+        iteracao++
         if(EC.escolhido == true){
           return LE
         }
         let filhosEC= semFilhos(EC,LE,LNE,BSS);
         if(filhosEC == true){
-          console.log("EC NÃO TEM FILHOS");
+          
           while(LE.length != 0 && EC.nome == LE[0].nome){
             BSS.unshift(EC);
             LE.shift();
@@ -124,21 +133,20 @@ let grafo = [
           LE.unshift(EC);
         }
         else{
-          console.log("primeiro filho de EC: " + filhosEC.length)
+       
           for(let k = filhosEC.length-1;k>=0;k--){
             let filho = retornaNoPorNome(filhosEC[k],grafo);
             LNE.unshift(filho);
-            console.log("FILHO DE EC: "+ filho.nome)
+            
           }
           EC=LNE[0];
           LE.unshift(EC)
         }
+       
       }
-      console.log("nó escolhido não encontrado")
-      return false
+       console.log("nó escolhido não encontrado")
+        return false
     }
     let caminho = procurarNo(grafo);
-    for(let i=0;i<caminho.length;i++){
-      console.log(caminho[i].nome)
-    }
+   imprimirDados(caminho)
   
